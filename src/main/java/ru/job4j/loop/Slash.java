76807406385df0b1e2f3d2ef0aacda167a991dd7
admin <admin>
2022-04-18 19:23:12 +0300

    5.9. Крест в псевдографике. [#177182 #289937] - исправление

--- src/main/java/ru/job4j/loop/Slash.java
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -5,8 +5,6 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
-                boolean left = true;
-                boolean right = false;
                 if (row == cell) {
                     System.out.print("0");
                 } else if (row + cell == size - 1) {
