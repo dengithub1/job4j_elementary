84c7024f591fc341e89716bac42be0636f7e980d
admin <admin>
2022-12-14 14:51:05 +0300

    6.4.0. Слово начинается с ... [#41585 #370790] исправление

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,15 +2,14 @@ package ru.job4j.array;
 
 public class ArrayChar {
 
-        public static boolean startsWith(char[] word, char[] pref) {
-            boolean result = true;
-            int minLength = Math.min(word.length, pref.length);
-            for (int i = 0; i < minLength; i++) {
-                if (word[i] != pref[i]) {
-                    result = false;
-                    break;
-                }
-                }
-            return result;
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
+                result = false;
+                break;
+            }
         }
+        return result;
+    }
 }
