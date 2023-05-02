84049f1556b72d0d55cc507adba978f0372350a9
admin <desantt8@gmail.com>
2023-05-02 19:52:18 +0300

    6.6.1. Размер элемента двухмерного массива. [#333583] Поправил пустые строки Перенес из тест в мейн

--- src/main/java/ru/job4j/array/LengthArrayArrays.java
+++ src/main/java/ru/job4j/array/LengthArrayArrays.java
@@ -3,9 +3,7 @@ package ru.job4j.array;
 public class LengthArrayArrays {
 
     public static void main(String[] args) {
-
         int[][] numbers = {{4}, {4, 5}, {4, 5, 6}, {4, 5, 6, 7}};
-
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("Размер вложенного массива: " + numbers[i].length);
         }
